e1fc9c8e6631bbcac6d5e6a9152071478f7acd0b
webhero-nur <nurealom522@gmail.com>
2022-10-22 10:51:17 +0600

    Show All Button, Dynamic Data Load, Keyboard Enter Button Event Handler

--- js/app.js
+++ js/app.js
@@ -1,16 +1,22 @@
-const loadPhones = async (search = 'iphone') => {
-    url = `https://openapi.programming-hero.com/api/phones?search=${search}`;
+const loadPhones = async (searchText, dataLimit) => {
+    url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
     const res = await fetch(url);
     const data = await res.json();
-    displayPhone(data.data);
+    displayPhone(data.data, dataLimit);
 }
 
-const displayPhone = phones => {
+const displayPhone = (phones, dataLimit) => {
     const phonesContainer = document.getElementById('phones-container');
     phonesContainer.textContent = ``;
     // display 10 results only
-    phones = phones.slice(0, 10);
-
+    const showAll = document.getElementById('show-all');
+    if (dataLimit && phones.length >= 10) {
+        phones = phones.slice(0, 10);
+        showAll.classList.remove('d-none')
+    }
+    else {
+        showAll.classList.add('d-none');
+    }
 
     // display no result found message
     const noPhone = document.getElementById('no-found-message');
@@ -25,12 +31,12 @@ const displayPhone = phones => {
         const phoneDiv = document.createElement('div');
         phoneDiv.classList.add('col');
         phoneDiv.innerHTML = `
-            <div class="card p-4">
+            <div class="card p-2">
             <img src="${phone.image}" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">${phone.phone_name}</h5>
-                    <p class="card-text">This is a longer card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit longer.</p>
+                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <button onclick="loadPhoneDetailes('${phone.slug}')" href="#" class="btn btn-primary">Go somewhere</button>
                 </div>
             </div>
         `;
@@ -42,16 +48,27 @@ const displayPhone = phones => {
     toggleSpinner(false)
 }
 
-document.getElementById('btn-search').addEventListener('click', function () {
+const processSearch = (dataLimit) => {
     // start loading spinner
     toggleSpinner(true);
     hideResultContainer(false);
 
-    const searchText = document.getElementById('search-field');
-    const search = searchText.value;
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
     searchText.value = ``;
-    loadPhones(search);
+    loadPhones(searchText, dataLimit);
+}
 
+// handle search button click
+document.getElementById('btn-search').addEventListener('click', function () {
+    processSearch(10);
+});
+
+// search input field enter key handler
+document.getElementById('search-field').addEventListener('keyup', function (e) {
+    if (e.key === 'Enter') {
+        processSearch(10);
+    }
 });
 
 const toggleSpinner = isLoading => {
@@ -72,6 +89,18 @@ const hideResultContainer = isHidden => {
     else {
         resultContainer.classList.remove('d-none');
     }
+}
+
+// not the best way to load show all
+document.getElementById('btn-show-all').addEventListener('click', function () {
+    processSearch();
+});
+
+const loadPhoneDetailes = async id => {
+    const url = ` https://openapi.programming-hero.com/api/phone/${id}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data.data.name);
 }
 
 // loadPhones();
